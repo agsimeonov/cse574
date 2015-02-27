926a406a3649abbb9ee2d7725cb9e14be842e286
Luigi Di Tacchio <luigidit@buffalo.edu>
2015-02-26 20:05:54 -0500

    Fixed nnPredict

--- nnScript.py
+++ nnScript.py
@@ -211,7 +211,6 @@ def nnObjFunction(params, *args):
     #Your code here
     for 
     
-    
     #Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
     #you would use code similar to the one below to create a flat array
     obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
